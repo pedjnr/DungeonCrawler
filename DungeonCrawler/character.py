fb6db3fd170f4afd2e56c1a3268d03a6cc23f2cd
Pedro Junior <ped.jnr@gmail.com>
2025-03-29 11:03:49 -0300

    added multiple animation types (added both running and idle animation)

--- DungeonCrawler/character.py
+++ DungeonCrawler/character.py
@@ -7,13 +7,18 @@ class Character:
         self.flip = False
         self.animation_list = animation_list
         self.frame_index = 0
+        self.action = 0 #0: idle animation, 1: running animation
         self.update_time = pygame.time.get_ticks()
-        self.image = animation_list[self.frame_index]
+        self.running = False
+        self.image = animation_list[self.action][self.frame_index]
         #coordinates x and Y and width and height of the character
         self.rect = pygame.Rect(0, 0, 40, 40)
         self.rect.center = (x, y)
 
     def move(self, dx, dy):
+        self.running = False
+        if dx != 0 or dy != 0:
+            self.running = True
         if dx < 0:
             self.flip = True
         if dx > 0:
@@ -27,17 +32,32 @@ class Character:
         self.rect.y += dy
 
     def update(self):
+
+        #Checking what action the player is performing
+        if self.running == True:
+            self.update_action(1) #Running animation
+        else:
+            self.update_action(0) #Idle animation
+
         animation_cooldown = 70
         #Handle animation
         #Update_image
-        self.image = self.animation_list[self.frame_index]
+        self.image = self.animation_list[self.action][self.frame_index]
         #checking if enough time has passed since the last update
         if pygame.time.get_ticks() - self.update_time > animation_cooldown:
             self.frame_index += 1
             self.update_time = pygame.time.get_ticks()
         #Checking if the animation has finished
-        if self.frame_index >= len(self.animation_list):
+        if self.frame_index >= len(self.animation_list[self.action]):
             self.frame_index = 0
+
+    def update_action(self, new_action):
+        #Checking if the new action is different from the previous one
+        if new_action != self.action:
+            self.action = new_action
+            #Updating the animation settings
+            self.frame_index = 0
+            self.update_time = pygame.time.get_ticks()
 
     def draw (self, surface):
         flipped_image = pygame.transform.flip(self.image, self.flip, False)
--- DungeonCrawler/main.py
+++ DungeonCrawler/main.py
@@ -24,13 +24,21 @@ def scale_img(image, scale):
     h = image.get_height()
     return pygame.transform.scale(image, (w * scale, h * scale))
 
+
+animation_types = ["idle", "run"]
+#Loading the images
 animation_list = []
-for i in range(4):
-    #Loading the player image into the game
-    img = pygame.image.load(f"assets/images/characters/elf/idle/{i}.png").convert_alpha()
-    #Scaling the player according to the scale constant
-    img = scale_img(img, constants.SCALE)
-    animation_list.append(img)
+for animation in animation_types:
+    #Reseting temporary list of images
+    temp_list = []
+    for i in range(4):
+        #Loading the player image into the game
+        img = pygame.image.load(f"assets/images/characters/elf/{animation}/{i}.png").convert_alpha()
+        #Scaling the player according to the scale constant
+        img = scale_img(img, constants.SCALE)
+        temp_list.append(img)
+    animation_list.append(temp_list)
+
 
 #Creating the player
 player = Character(100, 100, animation_list)
